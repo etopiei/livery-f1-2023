0b855afc68cdd13ab262b869af31172abb868a83
etopiei <lj343@icloud.com>
2023-02-27 20:18:07 +1100

    Add mercedes and haas, and download script.

--- check.sh
+++ check.sh
@@ -1,14 +1,8 @@
 alpine=`curl --silent https://www.formula1.com/content/dam/fom-website/teams/2023/alpine.png | sha1sum | awk '{print $1}'`
 [ $alpine != "092e7cff30fabc9e3a30f76d1a709a19cbe1c492" ] && echo "Alpine ready!"
 
-mercedes=`curl --silent https://www.formula1.com/content/dam/fom-website/teams/2023/mercedes.png | sha1sum | awk '{print $1}'`
-[ $mercedes != "a95c56b5c5ffaad59b69245b6093d5c9e4334099" ] && echo "Mercedes ready!"
-
 ferrari=`curl --silent https://www.formula1.com/content/dam/fom-website/teams/2023/ferrari.png | sha1sum | awk '{print $1}'`
 [ $ferrari != "a17812d55ac56462a040ee5bf6e351bfecce5abb" ] && echo "Ferrari ready!"
-
-haas=`curl --silent https://www.formula1.com/content/dam/fom-website/teams/2023/haas-f1-team.png | sha1sum | awk '{print $1}'`
-[ $haas != "f53022723e2a1178a8ad23abb29e2e671ec0091d" ] && echo "Haas ready!"
 
 redbull=`curl --silent https://www.formula1.com/content/dam/fom-website/teams/2023/red-bull-racing.png | sha1sum | awk '{print $1}'`
 [ $redbull != "2e014bcf8fb8c47619f5777e583ea55d229114ce" ] && echo "Red bull ready!"
---
+++ download.sh
@@ -0,0 +1,11 @@
+year="2023"
+
+#curl --silent https://www.formula1.com/content/dam/fom-website/teams/$year/alpine.png > alpine.png
+#curl --silent https://www.formula1.com/content/dam/fom-website/teams/$year/mercedes.png > mercedes.png
+#curl --silent https://www.formula1.com/content/dam/fom-website/teams/$year/ferrari.png > ferrari.png
+#curl --silent https://www.formula1.com/content/dam/fom-website/teams/$year/haas-f1-team.png > haas.png
+#curl --silent https://www.formula1.com/content/dam/fom-website/teams/$year/mclaren.png > mclaren.png
+#curl --silent https://www.formula1.com/content/dam/fom-website/teams/$year/red-bull-racing.png > redbull.png
+#curl --silent https://www.formula1.com/content/dam/fom-website/teams/$year/aston-martin.png > aston-martin.png
+#curl --silent https://www.formula1.com/content/dam/fom-website/teams/$year/alphatauri.png > alphatauri.png
+#curl --silent https://www.formula1.com/content/dam/fom-website/teams/$year/williams.png > williams.png
